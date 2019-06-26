e0ec16593e5ff7c9cb4f7d83a98a864d95ae3742
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-06-26 19:06:29 +0300

    Tiny change in the Node.js example program

--- examples/timers.js
+++ examples/timers.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const ah = require('async_hooks');
 
-function init(asyncId, type, trigger, resource) {  }
+function init(asyncId, type, trigger, resource) {
+  fs.writeSync(1, `Create ${asyncId}, ${type}\n`);
+}
 function before(asyncID) {  }
 function after() {}
 function destroy() {}
