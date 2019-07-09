5834947e46f9444071f4ddb91d8e98d03d239e21
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-07-09 10:32:12 +0300

    CMake: Check that the expected file exists

--- tests/runtest.cmake
+++ tests/runtest.cmake
@@ -18,14 +18,19 @@ endif (NOT TEST_SUITE)
 message("Testing with DynamoRIO file: ${DYNAMO_FILE}")
 message("Test Binary: ${TEST_CMD}")
 
-string(REPLACE ".js" ".exp" filename ${TEST_FILE})
+# Determine the filename tha contains the expected output.
+string(REPLACE ".js" ".expf" filename ${TEST_FILE})
+# Get the absolute path of the file that holds the expected result.
 get_filename_component(expected_filename
-  "${CMAKE_BINARY_DIR}/../../tests/${TEST_SUITE}/${filename}"
+  "${CMAKE_SOURCE_DIR}/../../tests/${TEST_SUITE}/${filename}"
   REALPATH)
+# Get the absolute path of the file that contains the source
+# code of the program under test.
 get_filename_component(test_file
-  "${CMAKE_BINARY_DIR}/../../tests/${TEST_SUITE}/${TEST_FILE}"
+  "${CMAKE_SOURCE_DIR}/../../tests/${TEST_SUITE}/${TEST_FILE}"
   REALPATH)
 
+# Define the command to run.
 set (cmd ${DYNAMO_FILE} -c ../libfsracer.so -- ${TEST_CMD} ${test_file})
 
 # Running Test Application using DynamoRIO and capturing its standard output
@@ -52,14 +57,25 @@ string(REGEX REPLACE
 )
 file(WRITE output_file ${output_repl})
 
-# We use diff to compare the standard output of the previous.
-execute_process(
-  COMMAND diff output_file ${expected_filename}
-  ERROR_VARIABLE output
-  RESULT_VARIABLE exit_code
-)
 
-if (exit_code)
-  message(SEND_ERROR
-    "Test ${TEST_FILE} does not produce the expected output\n${output}")
-endif (exit_code) 
+if (EXISTS "${expected_filename}")
+  # The filename exists, so we use the `diff` utility to compare
+  # the expected result with the current one.
+  execute_process(
+    COMMAND diff output_file ${expected_filename}
+    OUTPUT_VARIABLE output
+    RESULT_VARIABLE exit_code
+  )
+
+  # The `diff` utility produced a non zero-exit code, so we presume
+  # that the files do not match.
+  if (exit_code)
+    message(SEND_ERROR
+      "Test ${TEST_FILE} does not produce the expected output\n${output}")
+  endif (exit_code) 
+else ()
+  # If the file does not exist, we just create the expected file
+  # and pass the current test.
+  # Note that this can be used for creating expected files efficiently.
+  file(WRITE "${expected_filename}" ${output_repl})
+endif ()
