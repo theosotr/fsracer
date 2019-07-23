5223af33279b9d0d02fc66c2c23f1e815810c433
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-07-23 12:23:29 +0300

    Test streams

--- tests/node_tests/stream.js
+++ tests/node_tests/stream.js
@@ -1,16 +1,10 @@
 const fs = require('fs');
-const ah = require('async_hooks')
-function init(a, b, c) {
-  fs.writeSync(1, `${a}, ${b}, ${c}\n`);
-}
-function before(a) {
-  fs.writeSync(1, `Before ${a}\n`);
-}
-function after() {  }
-function destroy() {  }
-function resolve() {  }
-ah.createHook({ init, before, after, destroy, resolve }).enable()
+
 
 fs.writeFileSync('foo', 'line1\nline2');
-fs.createReadStream('foo')
+var stream = fs.createReadStream('foo')
   .pipe(fs.createWriteStream('bar'));
+stream.on('close', () => {
+  fs.unlinkSync('foo');
+  fs.unlinkSync('bar');
+})
