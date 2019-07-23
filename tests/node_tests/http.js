90f0b3effc52d6a6f66a444f9b8beb5d870e5c50
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-07-23 12:06:35 +0300

    Test trace generator in http requests

--- tests/node_tests/http.js
+++ tests/node_tests/http.js
@@ -1,27 +1,10 @@
 const fs = require('fs');
-const ah = require('async_hooks');
-function init(a, b, c) {
-  fs.writeSync(1, `${a}, ${b}, ${c}\n`);
-}
-function before(a) {
-  fs.writeSync(1, `Before ${a}\n`);
-}
-function after(a) {
-  fs.writeSync(1, `After ${a}\n`);
-}
-function destroy() {  }
-function resolve() {  }
-ah.createHook({ init, before, after, destroy, resolve }).enable()
-
 const http = require('http');
 
-http.get('http://google.com', (res) => {
-  fs.writeSync(1, 'Request 1...\n');
-  fs.writeFile('foo', 'data', () => {});
-})
-
-
-http.get('http://google.com', (res) => {
-  fs.writeSync(1, 'Request 2...\n');
-  fs.access('foo', () => {});
-})
+http.get('http://google.com', () => {
+  http.get("http://google.com", () => {
+    fs.writeFile('foo', 'data', () => {
+      fs.unlinkSync("foo");
+    });
+  })
+});
