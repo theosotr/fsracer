381c66355140ef7335e195ee476ed801d4070b81
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-08-14 20:35:05 +0300

    Minor stylish changes

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -11,53 +11,54 @@
 
 namespace utils {
 
-  std::string PtrToString(const void *ptr) {
-    std::stringstream ss;
-    ss << ptr;
-    return ss.str();
+
+std::string PtrToString(const void *ptr) {
+  std::stringstream ss;
+  ss << ptr;
+  return ss.str();
+}
+
+
+size_t GetCurrentThread(void *wrapctx) {
+  void *drcontext = drwrap_get_drcontext(wrapctx);
+  return dr_get_thread_id(drcontext);
+}
+
+
+std::string GetRightSubstr(std::string &str, std::string delm) {
+  size_t pos = str.find(delm);
+  if (pos == std::string::npos) {
+    return str;
   }
+  return str.substr(pos + 1, std::string::npos);
+}
 
 
-  size_t GetCurrentThread(void *wrapctx) {
-    void *drcontext = drwrap_get_drcontext(wrapctx);
-    return dr_get_thread_id(drcontext);
-  }
+void timer::Start() {
+  start_time = std::chrono::high_resolution_clock::now();
+}
 
 
-  std::string GetRightSubstr(std::string &str, std::string delm) {
-    size_t pos = str.find(delm);
-    if (pos == std::string::npos) {
-      return str;
-    }
-    return str.substr(pos + 1, std::string::npos);
-  }
+void timer::Stop() {
+  auto elapsed = std::chrono::high_resolution_clock::now() - start_time;
+  time = std::chrono::duration_cast<std::chrono::microseconds>(
+      elapsed).count();
+}
 
 
-  void timer::Start() {
-    start_time = std::chrono::high_resolution_clock::now();
-  }
+double timer::GetTimeMillis() const {
+  return (double) time / 1000;
+}
 
 
-  void timer::Stop() {
-    auto elapsed = std::chrono::high_resolution_clock::now() - start_time;
-    time = std::chrono::duration_cast<std::chrono::microseconds>(
-        elapsed).count();
-  }
+double timer::GetTimeSeconds() const {
+  return (double) time / 1000000;
+}
 
 
-  double timer::GetTimeMillis() const {
-    return (double) time / 1000;
-  }
-
-
-  double timer::GetTimeSeconds() const {
-    return (double) time / 1000000;
-  }
-
-
-  double timer::GetTimeMicros() const {
-    return time;
-  }
+double timer::GetTimeMicros() const {
+  return time;
+}
 
 
 } // namespace utils
--- src/Utils.h
+++ src/Utils.h
@@ -10,47 +10,47 @@
 namespace utils {
 
 
-  std::string PtrToString(const void *ptr);
+std::string PtrToString(const void *ptr);
 
-  size_t GetCurrentThread(void *wrapctx);
+size_t GetCurrentThread(void *wrapctx);
 
-  std::string GetRightSubstr(std::string &str, std::string delm);
+std::string GetRightSubstr(std::string &str, std::string delm);
 
-  template<template<typename> class C, typename T>
-  std::vector<std::pair<T, T>> Get2Combinations(const C<T> &a) {
-    std::vector<std::pair<T, T>> combs;
-    for (typename C<T>::const_iterator it = a.begin(); it != a.end(); it++) {
-      for (auto it_c = it; it_c != a.end(); it_c++) {
-        combs.push_back({ *it, *it_c });
-      }
+template<template<typename> class C, typename T>
+std::vector<std::pair<T, T>> Get2Combinations(const C<T> &a) {
+  std::vector<std::pair<T, T>> combs;
+  for (typename C<T>::const_iterator it = a.begin(); it != a.end(); it++) {
+    for (auto it_c = it; it_c != a.end(); it_c++) {
+      combs.push_back({ *it, *it_c });
     }
-    return combs;
   }
+  return combs;
+}
 
-  /** Class that tracks the time interval between two timer periods. */
-  class timer {
-  public:
-    /** Start tracking time. */
-    void Start();
+/** Class that tracks the time interval between two timer periods. */
+class timer {
+public:
+  /** Start tracking time. */
+  void Start();
 
-    /** Stop tracking time. */
-    void Stop();
+  /** Stop tracking time. */
+  void Stop();
 
-    /** Get time interval in milli seconds. */
-    double GetTimeMillis() const;
+  /** Get time interval in milli seconds. */
+  double GetTimeMillis() const;
 
-    /** Get time interval in seconds. */
-    double GetTimeSeconds() const;
+  /** Get time interval in seconds. */
+  double GetTimeSeconds() const;
 
-    /** Get time interval in micro seconds. */
-    double GetTimeMicros() const;
+  /** Get time interval in micro seconds. */
+  double GetTimeMicros() const;
 
-  private:
-    /// Starting time point.
-    std::chrono::high_resolution_clock::time_point start_time;
-    /// Time interval in micro seconds.
-    long time;
-  };
+private:
+  /// Starting time point.
+  std::chrono::high_resolution_clock::time_point start_time;
+  /// Time interval in micro seconds.
+  long time;
+};
 
 } //namespace utils
 
