14a6b885c8e0467bf1286b02a4e72aefbb51c0a6
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-09-18 12:50:55 +0300

    Instrument entry point and run tests sequentially

--- scripts/test-modules.sh
+++ scripts/test-modules.sh
@@ -2,9 +2,66 @@
 
 modules=$(realpath $1)
 
+
+function enable_async_hooks()
+{
+  # Enable the async hooks by adding the necessary code at the beginning
+  # of the file that corresponds to the entry point of the package.
+  local preamble="const ah = require('async_hooks');\n \
+    function ahf() {  }\n \
+    ah.createHook({ ahf, ahf, ahf, ahf, ahf }).enable();\n";
+  local code="$preamble"
+  if [ -f index.js ];
+  then
+    sed -i "1s/^/${code}/" index.js
+    return 0
+  else
+    main=$(cat package.json | jq -r '.main')
+    if [ ! -z $main ];
+    then
+      return 1
+    fi
+    sed -i "1s/^/${code}/" $main
+    return 0
+  fi
+  return 1
+}
+
+
+function get_test_options()
+{
+  # Replace the test command so that we run all tests sequenatially.
+  # TODO: Support more testing frameworks.
+  local tcmd=$(cat package.json | jq -r '.scripts.test')
+  if [[ $tcmd == *"tap"* ]];
+  then
+    echo "-j 1"
+    return 0
+  elif [[ $tcmd == *"ava"* ]];
+  then
+    echo "--serial"
+    return 0
+  elif [[ $tcmd == *"jest"* ]];
+  then
+    echo "--runInBand --detectOpenHandles"
+    return 0
+  elif [[ $tcmd == *"mocha "* ]];
+  then
+    return 0
+  else
+    return 1;
+  fi
+}
+
+
+cp /dev/null warnings.txt
+cp /dev/null success.txt
+
+
 while IFS= read module
 do
-  metadata=$(curl -X GET "https://api.npms.io/v2/package/$module" |
+  echo "Processing $module..."
+  metadata=$(curl -s -X GET "https://api.npms.io/v2/package/$module" |
   jq -r '.collected.metadata')
 
   if echo "$metadata" | jq -e 'has("deprecated")' > /dev/null;
@@ -15,19 +72,40 @@ do
   if [ "true" = $(echo "$metadata" | jq -r ".hasTestScript") ];
   then
     repo=$(echo "$metadata" | jq -r ".links.repository")
-    printf "\nCloning $module...\n"
-    git clone $repo $module
+    echo "Cloning $module..."
+    git clone $repo $module > /dev/null 2>&1
     cd $module
 
-    printf "\nInstalling $module...\n"
+    enable_async_hooks
+    if [ $? -ne 0 ];
+    then
+      # We were not able to find the entry point of the package.
+      echo "$module: Unable to find its entry point" >> ../warnings.txt
+      cd ..
+      continue
+    fi
+
+    echo "Installing $module..."
     if [ ! -f package-lock.json ];
     then
-      npm i --package-lock-only
+      npm i --package-lock-only > /dev/null 2>&1
     fi
-    npm audit fix --force
-    npm install
+    npm audit fix --force > /dev/null 2>&1
+    npm install > /dev/null 2>&1
 
-    printf "\nTesting $module...\n"
-    npm test
+    echo "Testing $module..."
+
+    opts=$(get_test_options)
+    if [ $? -ne 0 ];
+    then
+      echo "$module: Unable to find the testing framework" >> ../warnings.txt
+      cd ..
+      continue
+    fi
+    eval "timeout -s KILL 2m npm test -- $opts"
+    echo "$module" >> ../success.txt
+    cd ..
   fi
 done < $modules
+
+exit 0
