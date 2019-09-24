4bac943bad9cb0a1b57a914a3ac33ab8595c0281
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-09-24 11:10:36 +0300

    Add script for spawning Docker containers

---
+++ scripts/use-docker.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+HOME=/home/fsracer
+output_dir=$(realpath $1)
+modules=$(realpath $2)
+dynamo_dir=$HOME/dynamo
+fsracer_dir=$HOME/fsracer/build/tools
+
+mkdir -p $output_dir
+
+sudo docker run \
+  -v $output_dir:$HOME/out \
+  -v $modules:$HOME/modules.txt fsracer /bin/bash \
+  -c $HOME/fsracer/scripts/collect-traces \
+  -m $HOME/modules.txt \
+  -d $dynamo_dir \
+  -f $fsracer_dir \
+  -o $HOME/out
