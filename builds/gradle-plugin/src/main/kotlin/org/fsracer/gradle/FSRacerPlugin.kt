589b1938404dbb348d074ed0c4c9d6e2a0d792c1
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-10-02 16:15:42 +0300

    Working on a multi-project gradle build

--- builds/gradle-plugin/src/main/kotlin/org/fsracer/gradle/FSRacerPlugin.kt
+++ builds/gradle-plugin/src/main/kotlin/org/fsracer/gradle/FSRacerPlugin.kt
@@ -4,6 +4,9 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 
+fun constructTaskName(task : Task) : String {
+    return ":" + task.project.name + ":" + task.name
+}
 
 class FSRacerPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -11,14 +14,16 @@ class FSRacerPlugin : Plugin<Project> {
             p.gradle.taskGraph.whenReady { taskGraph ->
                 println("Begin MAIN " + p.name)
                 taskGraph.allTasks.forEach { task ->
-                    println("newEvent " + task.name +" W 1")
+                    val taskName = constructTaskName(task)
+                    println("newEvent " + taskName +" W 1")
                     task.getTaskDependencies()
                       .getDependencies(task)
                       .forEach { d ->
-                          println("link " + d.name + " " + task.name)
+                          val depTask = constructTaskName(d)
+                          println("link " + depTask + " " + taskName)
                     }
                     task.doFirst {
-                        println("Begin " + task.name)
+                        println("Begin " + taskName)
                     }
                     task.doLast {
                         println("End")
