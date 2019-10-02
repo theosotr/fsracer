1dcd116f0f8c712b26936d75048bd0e93195c431
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-10-02 14:14:57 +0300

    First steps on gradle plugin

---
+++ builds/gradle-plugin/src/main/kotlin/org/fsracer/gradle/FSRacerPlugin.kt
@@ -0,0 +1,17 @@
+package org.fsracer.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class FSRacerPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        project.allprojects { p ->
+            p.gradle.taskGraph.whenReady { taskGraph ->
+                taskGraph.allTasks.forEach { task ->
+                    println(task)
+                }
+            }
+        }
+    }
+}
