3849cf513c4a03518450773d3b2a2f31e3e646cf
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-10-02 15:48:20 +0300

    Emit newEvent and link expressions

--- builds/gradle-plugin/src/main/kotlin/org/fsracer/gradle/FSRacerPlugin.kt
+++ builds/gradle-plugin/src/main/kotlin/org/fsracer/gradle/FSRacerPlugin.kt
@@ -2,21 +2,29 @@ package org.fsracer.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 
 
 class FSRacerPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.allprojects { p ->
             p.gradle.taskGraph.whenReady { taskGraph ->
+                println("Begin MAIN " + p.name)
                 taskGraph.allTasks.forEach { task ->
-                    println(task)
+                    println("newEvent " + task.name +" W 1")
+                    task.getTaskDependencies()
+                      .getDependencies(task)
+                      .forEach { d ->
+                          println("link " + d.name + " " + task.name)
+                    }
                     task.doFirst {
-                        println("Begin" + task.name)
+                        println("Begin " + task.name)
                     }
                     task.doLast {
                         println("End")
                     }
-                }
+               }
+               println("End")
             }
         }
     }
