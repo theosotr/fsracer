610669e4884bac3fa41083e94be6e36a5c9ff044
Stefanos Chaliasos <stefanoshaliassos@gmail.com>
2019-10-10 12:32:55 +0300

    Add nested example

---
+++ builds/make-plugin/playground/nested_example/main.c
@@ -0,0 +1,8 @@
+#include "nested_dir/s1.h"
+#include "nested_dir/s2.h"
+
+int main() {
+	s1_fun();
+	s2_fun();
+	return(0);
+}
---
+++ builds/make-plugin/playground/nested_example/nested_dir/s2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "s2.h"
+
+void s2_fun(void) {
+	printf("Hello s2!\n");
+	return;
+}
