d655bcb1690c654918554be160042d216e79fd61
Stefanos Chaliasos <stefanoshaliassos@gmail.com>
2019-10-17 17:56:51 +0300

    Fix end-to-end tests for make

--- builds/tools/tests/test_fstraces.py
+++ builds/tools/tests/test_fstraces.py
@@ -2,13 +2,16 @@ from .. import adapter
 from tempfile import NamedTemporaryFile
 
 
-def helper_fun(in_traces, fstraces):
+PATH = '/home/user/fsracer/builds/make-plugin/playground/'
+
+def helper_fun(in_traces, fstraces, example):
+    work_dir = PATH + example
     with open(in_traces, 'r') as f:
         traces = f.readlines()
     with open(fstraces, 'r') as f:
         expected = f.readlines()
     with NamedTemporaryFile('w') as f:
-        adapter.main(traces, f, "Make")
+        adapter.main(traces, f, "make", work_dir)
         f.flush()
         with open(f.name, 'r') as inp:
             fstraces = inp.readlines()
@@ -18,12 +21,22 @@ def helper_fun(in_traces, fstraces):
 
 
 def test_simple_make():
-    helper_fun('tests/data/simple.traces', 'tests/data/simple.fstraces')
+    helper_fun(
+        'tests/data/simple.traces', 'tests/data/simple.fstraces', 'simple'
+    )
 
 
 def test_multiline_make():
-    helper_fun('tests/data/multiline.traces', 'tests/data/multiline.fstraces')
+    helper_fun(
+        'tests/data/multiline.traces',
+        'tests/data/multiline.fstraces',
+        'multiple_lines'
+    )
 
 
 def test_nested_make():
-    helper_fun('tests/data/nested.traces', 'tests/data/nested.fstraces')
+    helper_fun(
+        'tests/data/nested.traces',
+        'tests/data/nested.fstraces',
+        'nested_example'
+    )
