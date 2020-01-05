bcf85c3fa93797b9d7349fa785ba4a067c8f7dc4
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-01-05 12:47:31 +0200

    Print analysis time per project

--- builds/tools/times.py
+++ builds/tools/times.py
@@ -2,12 +2,15 @@
 
 
 import fileinput
+import statistics
 
 times_p = []
 for line in fileinput.input():
-    _, times = line.split(',', 1)
+    p, times = line.split(',', 1)
     times = [float(x) for x in times.split(',')]
-    times_p.append(sum(times))
+    p_time = sum(times)
+    times_p.append(p_time)
+    print (p, p_time)
 
 
-print (sum(times_p) / len(times_p))
+print (sum(times_p) / len(times_p), statistics.median(times_p))
